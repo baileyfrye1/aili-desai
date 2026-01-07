60ea7db46137ef712b9048f4004c9c1ed93a891d
Bailey Frye <baileyafrye@comcast.net>
2026-01-07 11:37:41 -0600

    Reimplemented API route to allow client side to fetch songs without needing a full redeploy. New songs and deleted songs are automatically reflected

---
+++ src/pages/api/tracks.json.ts
@@ -0,0 +1,38 @@
+import { type Song } from "../../types/Song.ts";
+import type { APIRoute } from "astro";
+import { createClient } from "@sanity/client";
+
+const client = createClient({
+  projectId: "2jujrh8z",
+  dataset: "production",
+  apiVersion: "2026-01-07",
+  useCdn: false,
+});
+
+async function fetchSongs() {
+  return client.fetch<Song[]>(`*[_type=="song"] | order(_createdAt desc){
+_id,
+title,
+"audioUrl": song.asset->url
+}`);
+}
+
+export const GET: APIRoute = async ({ url }) => {
+  const invalidate = url.searchParams.get("invalidateCache") === "true";
+
+  if (invalidate) {
+    return new Response(JSON.stringify(await fetchSongs()), {
+      headers: {
+        "Content-Type": "application/json",
+        "Cache-Control": "no-store",
+      },
+    });
+  }
+
+  return new Response(JSON.stringify(await fetchSongs()), {
+    headers: {
+      "Content-Type": "application/json",
+      "Cache-Control": "public, max-age=3600",
+    },
+  });
+};
